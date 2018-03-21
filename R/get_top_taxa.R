2c0240629c67ba8cf19d68e09ce2c73161a9f403
gmteunisse <gmteunisse@gmail.com>
2018-03-21 16:55:29 -0300

    Fixed bugs with get_top_taxa.R

--- R/get_top_taxa.R
+++ R/get_top_taxa.R
@@ -22,32 +22,38 @@
 get_top_taxa <- function(physeq_obj, n, relative = TRUE, discard_other = FALSE, other_label = "Other"){
 
   #Define a temporary physeq object
-  ps_tmp <- physeq_obj
+  ps_tmpr <- physeq_obj
 
   #Rotate table if necessary
-  if (!taxa_are_rows(otu_table(ps_tmp))){
-    otu_table(ps_tmp) <- t(otu_table(ps_tmp))
+  if (!taxa_are_rows(otu_table(ps_tmpr))){
+    otu_table(ps_tmpr) <- t(otu_table(ps_tmpr))
   }
 
   #Use relative abundances if requested
   if (relative){
-    otu_table(ps_tmp) <- otu_table(ps_tmp) / sample_sums(ps_tmp)
+    smpl_sms <- sample_sums(ps_tmpr)
+    if (0 %in% smpl_sms){
+      warning("Warning: some samples contain 0 reads. These have been removed to avoid
+              downstream problems.")
+      ps_tmpr <- prune_samples(smpl_sms != 0, ps_tmpr)
+    }
+    otu_table(ps_tmpr) <- otu_table(ps_tmpr) / sample_sums(ps_tmpr)
   }
 
   #Get the top taxa names and discard or merge other taxa
-  abun_taxa <- names(sort(taxa_sums(ps_tmp), decreasing = TRUE)[1:n])
+  abun_taxa <- names(sort(taxa_sums(ps_tmpr), decreasing = TRUE)[1:n])
   if (discard_other){
-    ps_tmp <- prune_taxa(abun_taxa, ps_tmp)
+    ps_tmpr <- prune_taxa(abun_taxa, ps_tmpr)
   } else {
-    to_merge <- rownames(otu_table(ps_tmp))
+    to_merge <- rownames(otu_table(ps_tmpr))
     to_merge <- to_merge[!(to_merge %in% abun_taxa)]
-    ps_tmp <- merge_taxa(ps_tmp, to_merge, to_merge[1])
-    tax_tbl <- tax_table(ps_tmp)
+    ps_tmpr <- merge_taxa(ps_tmpr, to_merge)
+    tax_tbl <- tax_table(ps_tmpr)
     indx <- which(row.names(tax_tbl) %in% to_merge)
     tax_tbl[indx,] <- other_label
-    tax_table(ps_tmp) <- tax_tbl
+    tax_table(ps_tmpr) <- tax_tbl
   }
-  return(ps_tmp)
+  return(ps_tmpr)
 }
 
 
