65d5f29da847143fff82ee8a768e8109fcd858d4
UC Davis Chile <gmteunisse@gmail.com>
2018-03-22 12:14:06 -0300

    Fixed bug in get_top_taxa.R

--- R/get_top_taxa.R
+++ R/get_top_taxa.R
@@ -24,40 +24,44 @@ get_top_taxa <- function(physeq_obj, n, relative = TRUE, discard_other = FALSE, 
   #Define a temporary physeq object
   ps_tmp <- physeq_obj
 
-  #Rotate table if necessary
-  if (!taxa_are_rows(ps_tmp)){
-    tbl <- t(as.matrix(otu_table(ps_tmp)))
-    taxa_are_rows(tbl) <- TRUE
-    otu_table(ps_tmp) <- otu_table(tbl, taxa_are_rows = TRUE)
-  }
-
   #Check for 0 entries
   smpl_sms <- sample_sums(ps_tmp)
   if (0 %in% smpl_sms){
-    warning("Warning: some samples contain 0 reads. These have been removed to avoid
+    stop("Error: some samples contain 0 reads. These have to be removed to avoid
             downstream problems.")
-    ps_tmp <- prune_samples(smpl_sms != 0, ps_tmp)
+  }
+
+  #Extract the otu_table as a data.frame
+  otu_tbl <-otu_table(ps_tmp)
+  if (!taxa_are_rows(ps_tmp)){
+    otu_tbl <- t(otu_tbl)
   }
 
   #Use relative abundances if requested
   if (relative){
-    otu_table(ps_tmp) <- otu_table(ps_tmp) / sample_sums(ps_tmp)
+    otu_tbl <- apply(otu_tbl, 2, function(x){
+      x / sum (x)
+    })
   }
+
+  #Update the phyloseq object
+  otu_table(ps_tmp) <- otu_table(otu_tbl, taxa_are_rows = T)
 
   #Get the top taxa names and discard or merge other taxa
   abun_taxa <- names(sort(taxa_sums(ps_tmp), decreasing = TRUE)[1:n])
   if (discard_other){
-    ps_tmp <- prune_taxa(abun_taxa, ps_tmp)
+    physeq_obj <- prune_taxa(abun_taxa, physeq_obj)
   } else {
-    to_merge <- rownames(otu_table(ps_tmp))
+    to_merge <- taxa_names(physeq)
     to_merge <- to_merge[!(to_merge %in% abun_taxa)]
-    ps_tmp <- merge_taxa(ps_tmp, to_merge)
-    tax_tbl <- tax_table(ps_tmp)
+    print(to_merge)
+    physeq_obj <- merge_taxa(physeq_obj, to_merge)
+    tax_tbl <- tax_table(physeq_obj)
     indx <- which(row.names(tax_tbl) %in% to_merge)
     tax_tbl[indx,] <- other_label
-    tax_table(ps_tmp) <- tax_tbl
+    tax_table(physeq_obj) <- tax_tbl
   }
-  return(ps_tmp)
+  return(physeq_obj)
 }
 
 
